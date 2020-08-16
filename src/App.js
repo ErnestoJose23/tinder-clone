091fca69dbead94fb82151e2701379d253a9d093
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-16 13:07:44 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <h1>Hehe</h1>
+
       <Header />
 
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
-        <div>
+        <div className="header">
             <PersonIcon />
-
+            <img className="header__logo" src="https:/1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="tinder-logo" />
+            <ForumIcon />
         </div>
     )
 }
