43e31cf20a33aee5b35dc0b56cf2317b1243c488
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-16 13:30:28 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
       <Header />
-
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>Chat</h1>
+          </Route>
+          <Route path="/">
+            <h1>Homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
