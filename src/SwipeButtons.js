487202d98c895c66df18add2db3f80f2ba64cb41
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-18 12:52:43 +0200

    Firebase

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -6,15 +6,28 @@ import CloseIcon from "@material-ui/icons/Close";
 import StarRateIcon from "@material-ui/icons/StarRate";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
+import IconButton from "@material-ui/core/IconButton";
 
 const SwipeButtons = () => {
   return (
     <div className="swipeButtons">
-      <ReplayIcon fontSize="large" />
-      <CloseIcon fontSize="large" />
-      <StarRateIcon fontSize="large" />
-      <FavoriteIcon fontSize="large" />
-      <FlashOnIcon fontSize="large" />
+      <IconButton>
+        <ReplayIcon fontSize="large" className="replay" />
+      </IconButton>
+      <IconButton>
+        <CloseIcon fontSize="large" className="close" />
+      </IconButton>
+      <IconButton>
+        <StarRateIcon fontSize="large" className="star" />
+      </IconButton>
+      <IconButton>
+        {" "}
+        <FavoriteIcon fontSize="large" className="favourite" />
+      </IconButton>
+      <IconButton>
+        {" "}
+        <FlashOnIcon fontSize="large" className="flash" />
+      </IconButton>
     </div>
   );
 };
