a5ebfe716c9c6d5f7f4ddf79ab08385836b66eed
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-19 11:49:55 +0200

    Chat page

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import "./ChatScreen.css";
 
 function ChatScreen() {
   const [messages, setMessages] = useState([
@@ -6,20 +8,26 @@ function ChatScreen() {
       name: "hola",
       image:
         "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
-      message: "Whats updog?",
+      message: "Whats updodddg?",
     },
     {
       name: "hola",
       image:
         "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
-      message: "Whats updog?",
+      message: "Whats qweupdog?",
     },
-    { name: "hola", image: "...", message: "Whats updog?" },
+    { name: "hola", image: "...", message: "Whats upcsddog?" },
   ]);
 
   return (
     <div className="chatscreen">
       <p>YOU MATCHED WITH HOLA ON 10/08/20</p>
+      {messages.map((message) => (
+        <div className="message">
+          <Avatar className="image" alt={message.name} src={message.image} />
+          <p className="text">{message.message}</p>
+        </div>
+      ))}
     </div>
   );
 }
