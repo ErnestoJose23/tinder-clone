1d6b3ec78627f71b3d8caa88d708f83d6716b967
Ernesto Custodio <ernestojosecg@gmail.com>
2020-08-19 11:33:05 +0200

    Chat page

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import "./Chat.css";
 
-function Chat() {
-  return <div></div>;
+function Chat({ name, message, profilePic, timestamp }) {
+  return (
+    <div className="chat">
+      <Avatar className="image" alt={name} src={profilePic} />
+      <div className="details">
+        <h2>{name}</h2>
+        <p>{message}</p>
+      </div>
+      <p className="timestamp">{timestamp}</p>
+    </div>
+  );
 }
 
 export default Chat;
--- src/Chats.js
+++ src/Chats.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Chats.css";
+import Chat from "./Chat";
 
 function Chats() {
   return (
